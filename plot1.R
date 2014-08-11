05f179ab027fbb90c948fd0ca3b3ba25171944d7
MilliKilo <rubina.k.sattar@gmail.com>
2014-08-11 00:27:48 -0400

    Code for Project 2

--- plot1.R
+++ plot1.R
@@ -1,38 +1,16 @@
-##  1day = 24 * 60 = 1440 minutes.  Need for 2 days in feb, therefore 1440 * 2 = 2880
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
-fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download.file(fileUrl, destfile = "household_power_consumption.zip")
-unzip("household_power_consumption.zip")
-data <- read.table("household_power_consumption.txt", sep=";", header=TRUE, skip= 66636, nrow=2880)
+## Question 1: Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+## Information provided: Using the base plotting system, make a plot showing the total 
+## PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
 
-> names(data)
-[1] "X31.1.2007" "X23.59.00"  "X0.326"     "X0.126"     "X242.800"   "X1.400"     "X0.000"    
-[8] "X0.000.1"   "X0.000.2"  
-
-names(data)[1] <- "Date"
-names(data)[2] <- "Time"
-names(data)[3] <- "Global_active_power"
-names(data)[4] <- "Global_reactive_power"
-names(data)[5] <- "voltage"
-names(data)[6] <- "Global_intensity"
-names(data)[7] <- "Sub_metering_1"
-names(data)[8] <- "Sub_metering_2"
-names(data)[9] <- "Sub_metering_3"
-
-> names(data)
-[1] "Date"                  "Time"                  "Global_active_power"   "Global_reactive_power"
-[5] "voltage"               "Global_intensity"      "Sub_metering_1"        "Sub_metering_2"       
-[9] "Sub_metering_3"       
-
-qplot(Global_reactive_power, data = data, geom="histogram")
-
-r <- qplot(Global_active_power, data = data, binwidth = .5, geom="histogram", 
-           xlab="Global Active Power (kilowatts)", ylab="Frequency", main="Global Active Power", width=480, height=480, units="px")
-r + geom_histogram(binwidth = .5, color="black", fill="red")
-
+# Save png
 dev.copy(png,"plot1.png", width=480, height=480)
-png 
-6 
-> dev.off()
-RStudioGD 
-2 
+
+# Release screen device
+dev.off()
+
+
+## Answer 1: 
--- plot2.R
+++ plot2.R
@@ -1,36 +1,11 @@
-##  1day = 24 * 60 = 1440 minutes.  Need for 2 days in feb, therefore 1440 * 2 = 2880
+## Question 2: Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") 
+## from 1999 to 2008? 
+## Information provided: Use the base plotting system to make a plot answering this question.
 
-fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-download.file(fileUrl, destfile = "household_power_consumption.zip")
-unzip("household_power_consumption.zip")
+# Save png
+dev.copy(png,"plot2.png", width=480, height=480)
 
-names(data)
-##[1] "X31.1.2007" "X23.59.00"  "X0.326"     "X0.126"     "X242.800"   "X1.400"     "X0.000"    
-##[8] "X0.000.1"   "X0.000.2"  
-names(data)[1] <- "Date"
-names(data)[2] <- "Time"
-names(data)[3] <- "Global_active_power"
-names(data)[4] <- "Global_reactive_power"
-names(data)[5] <- "voltage"
-names(data)[6] <- "Global_intensity"
-names(data)[7] <- "Sub_metering_1"
-names(data)[8] <- "Sub_metering_2"
-names(data)[9] <- "Sub_metering_3"
-names(data)
-##[1] "Date"                  "Time"                  "Global_active_power"   "Global_reactive_power"
-##[5] "voltage"               "Global_intensity"      "Sub_metering_1"        "Sub_metering_2"       
-##[9] "Sub_metering_3"       
-
-data <- read.table("household_power_consumption.txt",sep=";",header=TRUE)
-data1 <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",]
-data1$Date <- as.character(data1$Date)
-data1$Time <- as.character(data1$Time)
-data1$Date <- rep(c("1/2/07","2/2/07"), each=1440)
-x <- paste(data1$Date,data1$Time)
-new.time <- strptime(x,"%d/%m/%y %H:%M:%S")
-data.new <- data.frame(new.time,data1)
-
-## plot2
-png(filename="plot2.png",width=480,height=480)
-plot2<-plot(new.time,as.numeric(data.new$Global_active_power)/1000,type="l", xlab="", ylab="Global Active Power(kilowatts)")
+# Release screen device
 dev.off()
+
+## Answer 2: 
